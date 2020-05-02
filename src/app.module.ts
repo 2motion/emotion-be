c288ad658cde7cea7512dc229d6f71f7776739d8
webhacking <a@hax0r.info>
2020-05-02 12:57:11 +0900

    chore: add authentication module and alias nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ArticleModule } from './article/article.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { AuthenticationModule } from './authentication/authentication.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), ArticleModule],
+  imports: [ConfigModule.forRoot(), ArticleModule, AuthenticationModule],
   controllers: [],
   providers: [],
 })
---
+++ src/shared/model/account.model.ts
@@ -0,0 +1,6 @@
+abstract class AccountModel {
+  public name: string;
+  public avatarUrl: string;
+}
+
+export default AccountModel;
