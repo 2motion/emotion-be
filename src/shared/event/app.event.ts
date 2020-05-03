00d69ce40e85ba1f762b209a44792abeb90e6154
webhacking <a@hax0r.info>
2020-05-03 12:55:26 +0900

    chore: delete nest event emitter

--- src/shared/event/app.event.ts
+++
@@ -1,9 +0,0 @@
-import { EventEmitter } from 'events';
-import { StrictEventEmitter } from 'nest-emitter';
-import { AccountEntity } from '@app/entities/account.entity';
-
-interface AppEvent {
-  createAccessToken: (account: AccountEntity) => string;
-}
-
-export type AppEventEmitter = StrictEventEmitter<EventEmitter, AppEvent>;
