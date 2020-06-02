e00d687c6c6dc2e27ceb5e73f7b7e56f509c630f
webhacking <a@hax0r.info>
2020-06-02 16:23:42 +0900

    test: solved me module test cases

--- src/me/me.controller.spec.ts
+++ src/me/me.controller.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MeController } from './me.controller';
+import { authenticationProvider } from '@app/authentication/authentication.provider';
+import { MeService } from './me.service';
+import { ConfigModule } from '@nestjs/config';
 
 describe('Me Controller', () => {
   let controller: MeController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule],
       controllers: [MeController],
+      providers: [MeService, ...authenticationProvider],
     }).compile();
 
     controller = module.get<MeController>(MeController);
--- src/me/me.service.spec.ts
+++ src/me/me.service.spec.ts
@@ -1,12 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MeService } from './me.service';
+import { authenticationProvider } from '@app/authentication/authentication.provider';
+import { ConfigModule } from '@nestjs/config';
 
 describe('MeService', () => {
   let service: MeService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MeService],
+      imports: [ConfigModule],
+      providers: [MeService, ...authenticationProvider],
     }).compile();
 
     service = module.get<MeService>(MeService);
